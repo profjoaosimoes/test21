ffef0cb5be1f4713fdd81a171d8bb0f147646440
profjoaosimoes <94000389+profjoaosimoes@users.noreply.github.com>
2021-11-16 08:26:42 +0000

    Add files via upload

---
+++ aula2_work_with_strings/work_with_strings/work_with_strings/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work_with_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Sou Pro do Rocket");
+
+            //Console.WriteLine("Sou Pro do \nRocket"); // \n paragraph
+
+            //Console.WriteLine("Sou Pro do \"Rocket\""); // \" print "
+
+            //Console.WriteLine("Sou Pro do \nRocket");
+
+            /*string player = "Sou noob do Rocket";
+            Console.WriteLine(player);*/
+
+            /*string player = "Sou noob do Rocket";
+            Console.WriteLine(player.Length);*/ //methods
+
+             /*string player = "Sou noob do Rocket";
+            Console.WriteLine(player.ToUpper());*/
+
+             string player = "Sou noob do Rocket";
+            Console.WriteLine(player.Contains("Rocket")); //params
+
+            /*string player = "Sou noob do Rocket";
+           Console.WriteLine(player[9]);*/ //index 
+
+            /*string player = "Sou noob do Rocket";
+            Console.WriteLine(player.IndexOf("noob"));*/ //index where "noob" starts
+
+            /*string player = "Sou noob do Rocket";
+            //Console.WriteLine(player.Substring(4)); //starts at index 4 
+            Console.WriteLine(player.Substring(4,6));*/ //starts at index 4 and shows the following 4 chars
+            
+            Console.ReadLine();
+        }
+    }
+}
